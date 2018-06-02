46982492607d08fd22c5a636efa4ddaa73b9db7a
Ashley <anoelj86@yahoo.com>
2018-06-02 16:18:48 -0600

    youtube api

--- assets/jiggy.js
+++ assets/jiggy.js
@@ -2,10 +2,58 @@ var mUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&" + apiKey
 var apiKey = "apikey=b9c0f031"
 var title = "";
 
+
+
+$("#search").on("click", function () {
+
+    var title = $("#input").val().trim();
+
+    $.ajax({
+        url: "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&" + apiKey,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    })
+
+})
+
 $.ajax({
-    url: "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&" + apiKey,
+    url: "https://www.youtube.com/embed?listType=search&list=QUERY",
     method: "GET"
 }).then(function (response) {
     console.log(response);
 })
+
+function init() {
+    gapi.client.setApiKey('AIzaSyAgdHAGfQ-cKmJhT-WqMdG8gv3MKVXRNP0');
+    gapi.client.load("youtube", "v3", function () {
+
+    });
+
+}
+
+
+
+// Load the IFrame Player API code asynchronously.
+<div id="ytplayer"></div>
+var DRIVE_UPLOAD_URL = 'https://www.youtube.com/embed?listType=search&list=QUERY';
+
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Replace the 'ytplayer' element with an <iframe> and
+// YouTube player after the API code downloads.
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+        height: '360',
+        width: '640',
+        videoId: 'M7lc1UVf-VE'
+    });
+}
+
+
 
