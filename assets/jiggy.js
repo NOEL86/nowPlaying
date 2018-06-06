d5d240ac213c2928b7dd22271004e1ce88ad1110
Ashley <anoelj86@yahoo.com>
2018-06-06 17:13:36 -0600

    deleted extras

--- assets/jiggy.js
+++ assets/jiggy.js
@@ -1,3 +1,7 @@
+var q = "";
+var mUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&type=movie&" + apiKey;
+var apiKey = "apikey=b9c0f031"
+var title = "Footloose";
 
 // $("#movie-search").on("click", function () {
 //     search();
@@ -14,48 +18,32 @@
 // function search() {
 
 // var title = $("#input").val().trim();
-var mUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&" + apiKey
-var apiKey = "apikey=b9c0f031"
-var title = "";
+// var q = $("#input-2").val().trim();
+
 
 $.ajax({
-    url: "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&" + apiKey,
+    url: "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&type=movie&" + apiKey,
     method: "GET"
 }).then(function (response) {
     console.log(response);
 });
 
-
-var q = "";
-var DRIVE_UPLOAD_URL = 'https://www.youtube.com/embed?listType=search&list=' + q;
-
-
-// function init() {
-//     gapi.client.setApiKey('AIzaSyAgdHAGfQ-cKmJhT-WqMdG8gv3MKVXRNP0');
-//     gapi.client.load("youtube", "v3", function () {
-//     });
-
-// }
-
 var ytube = "AIzaSyAgdHAGfQ-cKmJhT-WqMdG8gv3MKVXRNP0";
 $.ajax({
     url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=viewCount&pageToken=CAoQAA&q=${q}&type=video&key=${ytube}`,
     method: "GET",
-    // beforeSend: function (xhr) {
-    //     /* Authorization header */
-    //     xhr.setRequestHeader("Authorization", ytube)
 
-    // },
     error: function (jqXHR, textStatus, errorThrown) {
         console.log(errorThrown);
     }
 
 }).then(function (response) {
     console.log(response);
-})
+});
+
+$("#search-appears-here").append()
 
 //Load the IFrame Player API code asynchronously.
-
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -71,10 +59,6 @@ function onYouTubePlayerAPIReady() {
 //Replace the 'ytplayer' element with an < iframe > and
 //YouTube player after the API code downloads.
 
-
-// init();
-
-// };
 
 
 
