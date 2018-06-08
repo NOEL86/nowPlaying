419c0c50f106edd3274b7f3a202a8d0b7283dcf0
Ashley <anoelj86@yahoo.com>
2018-06-07 18:25:37 -0600

    working ombd

--- assets/jiggy.js
+++ assets/jiggy.js
@@ -1,7 +1,7 @@
-var q = "";
+var q = "footloose";
 var mUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&type=movie&" + apiKey;
 var apiKey = "apikey=b9c0f031"
-var title = "Footloose";
+var title = "footloose";
 
 // $("#movie-search").on("click", function () {
 //     search();
@@ -11,49 +11,76 @@ var title = "Footloose";
 //     search();
 // });
 
-// $("#type-search").on("click", function () {
+// $("#typeSearchButton").on("click", function () {
+
 //     search();
 // })
 
-// function search() {
+function search() {
+    // q = $("#typeSearch").val().trim();
+    // title = $("#typeSearch").val().trim();
 
-// var title = $("#input").val().trim();
-// var q = $("#input-2").val().trim();
+    // var title = $("#input").val().trim();
+    // var q = $("#input-2").val().trim();
 
-<<<<<<< HEAD
-=======
-    var DRIVE_UPLOAD_URL = 'https://www.youtube.com/embed?listType=search&list=Batman';
-    var q = "";
->>>>>>> 73ea02f7420e23f607c83a603096300e28f89bdf
+    // var DRIVE_UPLOAD_URL = 'https://www.youtube.com/embed?listType=search&list=Batman';
+    // var q = "";
 
-$.ajax({
-    url: "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&type=movie&" + apiKey,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-});
+    $.ajax({
+        url: "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&type=movie&rating=&" + apiKey,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
 
-var ytube = "AIzaSyAgdHAGfQ-cKmJhT-WqMdG8gv3MKVXRNP0";
-$.ajax({
-    url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=viewCount&pageToken=CAoQAA&q=${q}&type=video&key=${ytube}`,
-    method: "GET",
+        $("#rating").append(response.Rated);
+        console.log(response.Rated);
 
-    error: function (jqXHR, textStatus, errorThrown) {
-        console.log(errorThrown);
-    }
+        $("#reviewScore").append(response.Ratings[1].Source);
+        $("#reviewScore").append(response.Ratings[1].Value);
 
-<<<<<<< HEAD
-}).then(function (response) {
-    console.log(response);
-});
-=======
-    console.log(response);
+        console.log(response.Ratings);
+
+        $("#cast").append(response.Actors);
+        console.log(response.Actors);
+
+        $("#synopsis").append(response.Plot);
+        console.log(response.Plot)
+
+
+        $("#poster").attr("src", response.Poster);
+        console.log(response.Poster);
+
+
+        $("#synopsis2").append(response.Plot);
+
+
+
+    });
 
 };
 
->>>>>>> 73ea02f7420e23f607c83a603096300e28f89bdf
+function getTrailer() {
+    var ytube = "AIzaSyAgdHAGfQ-cKmJhT-WqMdG8gv3MKVXRNP0";
 
-$("#poster").append()
+    $.ajax({
+        url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=viewCount&pageToken=CAoQAA&q=${q}&trailer&type=video&key=${ytube}`,
+        method: "GET",
+
+        error: function (jqXHR, appendStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+
+
+
+
+
+    });
+
+};
+
+search();
+getTrailer();
+
 
 //Load the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
@@ -68,8 +95,7 @@ function onYouTubePlayerAPIReady() {
         videoId: 'M7lc1UVf-VE'
     });
 }
-//Replace the 'ytplayer' element with an < iframe > and
-//YouTube player after the API code downloads.
+
 
 
 
