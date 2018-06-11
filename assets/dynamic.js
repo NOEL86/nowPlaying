2fa543a708cda943d2acd78c4b662758f1c77f8c
Ashley <anoelj86@yahoo.com>
2018-06-11 16:53:09 -0600

    youtube fix and similar not working yet

--- assets/dynamic.js
+++ assets/dynamic.js
@@ -10,48 +10,51 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-
-var q = "";
 var apiKey = "apikey=b9c0f031"
 var title = "";
-
-function search() {
-
-    $.ajax({
-        url: "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&type=movie&rating=&" + apiKey,
-        method: "GET"
-    }).then(function (response) {
-
-        var title = response.Title;
-        var rating = response.Rated;
-        var imdbScore = response.Ratings[0].Source + ": " + response.Ratings[0].Value;
-        var rottenTomatoesScore = response.Ratings[1].Source + ": " + response.Ratings[1].Value;
-        var metaCrticScore = response.Ratings[2].Source + ": " + response.Ratings[2].Value;
-        var cast = response.Actors;
-        var synopsis = response.Plot;
-        var poster = response.Poster;
-
-        newMovie = {
-            title: title,
-            rating: rating,
-            imdbScore: imdbScore,
-            rottenTomatoesScore: rottenTomatoesScore,
-            metaCrticScore: metaCrticScore,
-            cast: cast,
-            synopsis: synopsis,
-            poster: poster,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-        };
-
-        console.log(newMovie);
-
-        database.ref().push(newMovie);
-
-    });
-
-};
-
+var id = 0;
 $(window).on('load', function () {
+
+    function search() {
+
+        $.ajax({
+            url: "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&type=movie&rating=&" + apiKey,
+            method: "GET"
+        }).then(function (response) {
+
+            var title = response.Title;
+            var rating = response.Rated;
+            var imdbScore = response.Ratings[0].Source + ": " + response.Ratings[0].Value;
+            var rottenTomatoesScore = response.Ratings[1].Source + ": " + response.Ratings[1].Value;
+            var metaCrticScore = response.Ratings[2].Source + ": " + response.Ratings[2].Value;
+            var cast = response.Actors;
+            var synopsis = response.Plot;
+            var poster = response.Poster;
+            var id = response.results[0].id;
+
+            newMovie = {
+                title: title,
+                rating: rating,
+                imdbScore: imdbScore,
+                rottenTomatoesScore: rottenTomatoesScore,
+                metaCrticScore: metaCrticScore,
+                cast: cast,
+                synopsis: synopsis,
+                poster: poster,
+                id: id,
+                dateAdded: firebase.database.ServerValue.TIMESTAMP
+            };
+
+            console.log(newMovie);
+
+            database.ref().push(newMovie);
+            getTrailer();
+            similarMovies();
+        });
+
+    };
+
+
 
     database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function (snapshot) {
 
@@ -70,6 +73,7 @@ $(window).on('load', function () {
         cast = snapshot.val().cast;
         synopsis = snapshot.val().synopsis;
         poster = snapshot.val().poster;
+        id = snapshot.val().id;
 
         $("#title").append(title);
         $("#rating").append(rating);
@@ -78,11 +82,14 @@ $(window).on('load', function () {
         $("#synopsis").append(synopsis);
         $("#poster").attr("src", poster);
 
+
         function getTrailer() {
             var ytube = "AIzaSyAgdHAGfQ-cKmJhT-WqMdG8gv3MKVXRNP0";
+            var title = snapshot.val().title;
+
 
             $.ajax({
-                url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=viewCount&pageToken=CAoQAA&q=${title}trailer&type=video&key=${ytube}`,
+                url: `https://www.googleapis.com/youtube/v3/search?i18nLanguages&part=snippet&hl=en_US&maxResults=5&order=viewCount&q=${title}officialtrailer&type=video&key=${ytube}`,
                 method: "GET",
 
             }).then(function (response) {
@@ -92,35 +99,57 @@ $(window).on('load', function () {
 
                 $("#ytPlayer").append("<iframe id=\"player\" type\"text/html\" width=\"100%\" height=\"350px\" src=\"\" frameborder=\"0\"></iframe>");
                 //button for next possibly
-                $("#player").attr('src', "https://www.youtube.com/embed/" + response.items[0].id.videoId + "?autoplay=1")
+                $("#player").attr('src', "https://www.youtube.com/embed/" + response.items[1].id.videoId + "?autoplay=1");
+
             });
 
         };
+
         getTrailer();
+        similarMovies();
 
+        function similarMovies() {
+            var queryURL = `https://api.themoviedb.org/3/movie/{movie_${id}}/similar?api_key=6364491e63695bac0f912490a6a5a3d8&language=en-US&page=1`;
 
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+
+                console.log(queryURL);
+                console.log(response);
+
+                for (i = 0; i < 3; i++) {
+                    var responseTitle = response.results[i].original_title;
+                    var id = "movie" + i;
+                    var similarID = '<li id="' + id + '" class="listID" data-name="' + responseTitle + '">' + responseTitle + '</li>';
+                    console.log(listID);
+
+                    if (response.results[i].original_language == "en") {
+                        $(".Similar").append(similarID);
+                    };
+
+                };
+            });
+        };
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
     });
 
+
+
+    // Search Button Function
+    $("#dynamicSearchButton").on("click", function (event) {
+        event.preventDefault();
+        title = $("#dynamicSearch").val().trim();
+        search();
+        $("#dynamicSearch").val("");
+
+    });
+
+
+
 });
-
-// Search Button Function
-$("#dynamicSearchButton").on("click", function (event) {
-    event.preventDefault();
-
-    q = $("#dynamicSearch").val().trim();
-    title = $("#dynamicSearch").val().trim();
-
-    search();
-    getTrailer();
-
-    $("#dynamicSearch").val("");
-
-});
-
-
-
 
 
 
